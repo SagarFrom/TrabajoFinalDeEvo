a9cfc41e7ecc4ff7c6ace8dc915ea8839853e655
unknown <sagarfrom2014@outlook.com>
2014-12-05 12:02:35 -0500

    <sagarfrom2014@outlook.com>

--- Pruebas/CinemaxCore/src/main/java/pe/edu/upc/evolucion/Cinemazcore/test/business/PeliculaBusinessNGTest.java
+++ Pruebas/CinemaxCore/src/main/java/pe/edu/upc/evolucion/Cinemazcore/test/business/PeliculaBusinessNGTest.java
@@ -1,7 +1,26 @@
 
 package pe.edu.upc.evolucion.Cinemazcore.test.business;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pe.edu.upc.evolucion.cinemaxdac.entity.Pelicula;
+
 
 public class PeliculaBusinessNGTest {
     
+    public PeliculaBusinessNGTest(){
+        
+    }
+    
+    private static Pelicula pelicula;
+    
+        @BeforeClass
+        public static void setUpClass() throws Exception{
+            System.out.println("Inicio Test PeliculaBusiness");
+        }
+        
+        @AfterClass
+        public static void tearDownClass() throws Exception {
+            System.out.println("Fin Test PeliculaBusiness");
+        }
 }
